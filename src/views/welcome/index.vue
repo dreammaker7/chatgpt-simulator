6a3f331de163ef2e930015c4727e4e82caaf5fe0
ryan <lau52320000@gmail.com>
2023-02-17 01:19:51 +0800

    eslint、prettier 配置优化，和一些 Welcome 样式调整

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
-import { NLayout, NCard, NSpace } from "naive-ui";
+import { NLayout, NLayoutHeader, NLayoutContent, NCard, NSpace } from "naive-ui";
 </script>
 
 <template>
-  <n-layout embedded content-style="padding: 24px;">
-    <n-space justify="center" align="center" style="height: 100%">
-      <n-card class="card">
-        <strong>Hi, Welcome to ChatGPT Simulator!</strong>
-      </n-card>
-    </n-space>
+  <n-layout :native-scrollbar="false" style="height: 100%" embedded>
+    <n-layout-header>暗色</n-layout-header>
+    <n-layout-content content-style="padding: 24px;">
+      <n-space justify="center" align="center">
+        <n-card class="card">
+          <strong>Hi, Welcome to ChatGPT Simulator!</strong>
+        </n-card>
+      </n-space>
+    </n-layout-content>
   </n-layout>
 </template>
 
 <style scoped>
-.n-layout {
-  height: 100%;
+.n-layout-header {
+  padding: 16px;
+}
+
+.n-space {
+  min-height: 50vh;
 }
 
 strong {
