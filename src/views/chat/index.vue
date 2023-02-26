e14cebf88955a3210547c6b2b3e1df440951024e
ryan <lau52320000@gmail.com>
2023-02-27 00:09:26 +0800

    install openai，第一个 ChatGPT API

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,3 +1,26 @@
-<template>welcome to chat page!</template>
+<script setup lang="ts">
+  import { Configuration, OpenAIApi } from 'openai';
+  import { onBeforeMount } from 'vue';
 
-<script setup lang="ts"></script>
+  onBeforeMount(async () => {
+    const configuration = new Configuration({
+      apiKey: 'xxx'
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'Q: Where is China? A:',
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ['\n']
+    });
+
+    console.log(response);
+  });
+</script>
+
+<template>welcome to chat page!</template>
